98677962d7493bef56fe5e4a2e4a1ea0419ad03c
RoshanSalian <roshansalian1997@gmail.com>
2017-10-18 18:48:54 +0530

    database

---
+++ infopage.php
@@ -0,0 +1,189 @@
+<html>
+    <head>
+        <title>
+            Dashboard
+        </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/infopage.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+<!--custom fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Heebo" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Cookie|Zilla+Slab+Highlight" rel="stylesheet">
+
+
+    </head>
+
+<body>
+<div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#"><img src="Stock/logo.jpg" height="40px" width="55px"></a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+</div>
+<!--    second nav bar ==================================================================-->
+<!--    <div>-->
+<!--    <ul class="nav justify-content-center">-->
+<!--  <li class="nav-item dropdown">-->
+<!--    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>-->
+<!--    <div class="dropdown-menu">-->
+<!--      <a class="dropdown-item" href="#">Action</a>-->
+<!--      <a class="dropdown-item" href="#">Another action</a>-->
+<!--      <a class="dropdown-item" href="#">Something else here</a>-->
+<!--      <div class="dropdown-divider"></div>-->
+<!--      <a class="dropdown-item" href="#">Separated link</a>-->
+<!--    </div>-->
+<!--  </li>-->
+<!--          <li class="nav-item dropdown">-->
+<!--    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>-->
+<!--    <div class="dropdown-menu">-->
+<!--      <a class="dropdown-item" href="#">Action</a>-->
+<!--      <a class="dropdown-item" href="#">Another action</a>-->
+<!--      <a class="dropdown-item" href="#">Something else here</a>-->
+<!--      <div class="dropdown-divider"></div>-->
+<!--      <a class="dropdown-item" href="#">Separated link</a>-->
+<!--    </div>-->
+<!--  </li>-->
+<!--          <li class="nav-item dropdown">-->
+<!--    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>-->
+<!--    <div class="dropdown-menu">-->
+<!--      <a class="dropdown-item" href="#">Action</a>-->
+<!--      <a class="dropdown-item" href="#">Another action</a>-->
+<!--      <a class="dropdown-item" href="#">Something else here</a>-->
+<!--      <div class="dropdown-divider"></div>-->
+<!--      <a class="dropdown-item" href="#">Separated link</a>-->
+<!--    </div>-->
+<!--  </li>-->
+<!--</ul>-->
+<!--    -->
+<!--    -->
+<!--    </div>-->
+<!-- banner ============================================   -->
+    <div class="container-fluid">
+
+        <div class="row" style="height: 500px">
+            <div class="parallax" style="background-image:url(Stock/home.jpeg)">
+             <div>
+                 <div>
+                     <h1><img src="Stock/logo.jpg" height="100px" width="130px"></h1>
+                     <h3 class="subhead">All your accounts in<br> one place</h3>
+                 </div>
+                 <div style="bottom: 10px">
+                     <a href="index.php"> <button type="submit" class="btn btn-danger">Register</button></a>
+                 <button type="submit" class="btn btn-warning">Try for Free</button>
+                </div>
+             </div>
+        </div>
+        </div>
+          <div class="row" style="padding-top:30px;padding-bottom:30px;background: #353e46">
+              <div class="col-md-2"></div>
+              <div class="col-md-2">
+                  <div class="card" >
+                      <img class="card-img-top" src="Stock/card1.png" alt="Card image cap">
+                      <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title</p>
+
+                      </div>
+                    </div>
+              
+              </div>
+              <div class="col-md-2">
+                  <div class="card" >
+                      <img class="card-img-top" src="Stock/card1.png" alt="Card image cap">
+                      <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title </p>
+
+                      </div>
+                    </div>
+              
+              </div>
+              <div class="col-md-2">
+                  <div class="card"">
+                      <img class="card-img-top" src="Stock/card1.png" alt="Card image cap">
+                      <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title </p>
+
+                      </div>
+                    </div>
+              
+              </div>
+              <div class="col-md-2">
+                  <div class="card">
+                      <img class="card-img-top" src="Stock/card1.png" alt="Card image cap">
+                      <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title </p>
+                      </div>
+                    </div>
+              
+              </div>
+
+<!--    <div class="col-md-2">
+                  <div class="card">
+                      <img class="card-img-top" src="Stock/card1.png" alt="Card image cap">
+                      <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+
+              </div>    -->
+
+
+
+
+              <div class="col-md-2"></div>
+                            
+
+        </div>
+<div class="row" style="height: 500px">
+    <div class="parallax" style="background-image:url(Stock/home.jpeg)">
+        <div>
+            <div>
+                <h1>Account360</h1>
+            </div>
+            <div style="bottom: 10px">
+                <button type="submit" class="btn btn-danger">Register</button>
+                <button type="submit" class="btn btn-warning">Register</button>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+<footer style="padding-top:30px;padding-bottom:30px;background: #353e46">
+    <p>Posted by: Hege Refsnes</p>
+    <p>Contact information: <a href="mailto:someone@example.com">
+            someone@example.com</a>.</p>
+</footer>
+    
+    
+</body>
+</html>
